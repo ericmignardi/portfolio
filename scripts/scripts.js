88fa6d2e43588e0381a40c3ed820a28b1113323f
Eric Mignardi <mignardi.e@gmail.com>
2025-04-23 12:03:25 -0400

    Minor changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,16 @@
-const mobileMenu = document.querySelector(".mobile__list");
+const mobileMenu = document.querySelector(".mobile-nav__list");
 const mobileToggle = document.querySelector(".nav__toggle");
+const mobileLinks = document.querySelectorAll(".mobile-nav__item-link");
+
 let isMobileOpen = false;
+
+function closeMobileMenu() {
+  isMobileOpen = false;
+  mobileToggle.classList.remove("fa-close");
+  mobileToggle.classList.add("fa-bars");
+  mobileMenu.style.display = "none";
+  document.body.style.overflowY = "auto";
+}
 
 mobileToggle.addEventListener("click", () => {
   isMobileOpen = !isMobileOpen;
@@ -10,9 +20,13 @@ mobileToggle.addEventListener("click", () => {
     mobileMenu.style.display = "flex";
     document.body.style.overflowY = "hidden";
   } else {
-    mobileToggle.classList.remove("fa-close");
-    mobileToggle.classList.add("fa-bars");
-    mobileMenu.style.display = "none";
-    document.body.style.overflowY = "auto";
+    closeMobileMenu();
   }
+});
+
+// 👇 Add this to close menu when a mobile link is clicked
+mobileLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    closeMobileMenu();
+  });
 });
